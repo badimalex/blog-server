569e944b6e1b37eb39a23856b0adb39546ef60b6
Dimitri Badicean <badimalex@gmail.com>
2017-05-22 09:16:52 +0200

    [seeds]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 keywords = %w(rose lily begonia sunflower violets)
 (1..10).each do |i|
-  Post.create(
+  Post.new(
     title: "Post #{keywords.sample}",
     description: "Post #{keywords.sample} description",
     author: "user_#{i}",
     likes: rand(1...10),
     image: "flower#{i}.jpg"
-  )
+  ).save(validate: false)
 end
